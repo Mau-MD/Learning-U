4a588191e19e6a147147a5c194e07c7638dc0548
Mauricio Munoz <maumd@fb.com>
2022-06-28 15:28:54 -0700

    Initial React Router Dom Setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import { Box } from "@chakra-ui/react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-  return <Box>Hi</Box>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
