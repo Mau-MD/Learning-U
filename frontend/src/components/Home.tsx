4504e30e651fcd4019afe3776e78ece96c852069
Mauricio Munoz <maumd@fb.com>
2022-07-03 18:09:09 -0700

    Adding Home Component

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,20 +1,42 @@
 import * as React from "react";
-import { Badge, Box, Button, Container, Heading, Text } from "@chakra-ui/react";
+import {
+  Badge,
+  Box,
+  CircularProgress,
+  CircularProgressLabel,
+  Container,
+  Heading,
+  Image,
+} from "@chakra-ui/react";
 import ResourceGroup from "./Hub/ResourceGroup";
+import Banner from "./Hub/Banner";
 
 const Home = () => {
   return (
-    <Container maxW={"container.xl"} mt="2rem">
-      <Heading as="h1" fontWeight={"bold"} fontSize={"4xl"}>
-        React
-      </Heading>
-      <Badge>Beginner</Badge>
-      <Box display={"flex"} flexDirection={"column"} gap={10} my={10}>
-        <ResourceGroup title="🎥 Recommended Videos" />
-        <ResourceGroup title="📚 Documentation to Read" />
-        <ResourceGroup title="🎯 Projects to do" />
-      </Box>
-    </Container>
+    <>
+      <Banner src="https://miro.medium.com/max/1838/1*uE1kDeKaMnZaZLLJzHm0QA.png" />
+      <Container maxW={"container.xl"} mt="2rem">
+        <Box display="flex" alignItems="center" justifyContent="space-between">
+          <Box>
+            <Heading as="h1" fontWeight="bold" fontSize="4xl">
+              React
+            </Heading>
+            <Badge>Beginner</Badge>
+          </Box>
+          <CircularProgress value={30} size={"60px"}>
+            <CircularProgressLabel>30%</CircularProgressLabel>
+          </CircularProgress>
+        </Box>
+        <Box display="flex" flexDirection="column" gap={10} my={10}>
+          <ResourceGroup title="🎥 Recommended Videos" kind="video" />
+          <ResourceGroup
+            title="📚 Documentation to Read"
+            kind="documentation"
+          />
+          <ResourceGroup title="🎯 Projects to do" kind="video" />
+        </Box>
+      </Container>
+    </>
   );
 };
 
