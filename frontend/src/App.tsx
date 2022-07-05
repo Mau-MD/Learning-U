242a039d357f9bf7eaa9e870455caba7ab7ca98e
Mauricio Munoz <maumd@fb.com>
2022-07-05 14:34:00 -0700

    Adding login route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import LoginIndex from "./components/Registration/LoginIndex";
 import RegisterIndex from "./components/Registration/RegisterIndex";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<RegisterIndex />} />
+        <Route path="/login" element={<LoginIndex />} />
       </Routes>
     </BrowserRouter>
   );
