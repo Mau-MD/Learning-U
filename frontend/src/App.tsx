90462e0c3583b5c36609d3733dcd795c36956768
Mauricio Munoz <maumd@fb.com>
2022-07-05 14:08:32 -0700

    Adding register route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import RegisterIndex from "./components/Registration/RegisterIndex";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/register" element={<RegisterIndex />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/components/Registration/RegisterIndex.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RegisterIndex = () => {
+  return <div>RegisterIndex</div>;
+};
+
+export default RegisterIndex;
