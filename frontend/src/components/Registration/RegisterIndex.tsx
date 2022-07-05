6ce44f69c0c1c00c6d657d9b8492c225328e1455
Mauricio Munoz <maumd@fb.com>
2022-07-05 14:17:17 -0700

    Adding registration form

--- frontend/src/components/Registration/RegisterIndex.tsx
+++ frontend/src/components/Registration/RegisterIndex.tsx
@@ -1,7 +1,48 @@
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+} from "@chakra-ui/react";
 import React from "react";
+import useThemeColor from "../../hooks/useThemeColor";
 
 const RegisterIndex = () => {
-  return <div>RegisterIndex</div>;
+  const { backgroundColor, borderColor } = useThemeColor();
+  return (
+    <Center h="90vh">
+      <Flex
+        backgroundColor={backgroundColor}
+        borderColor={borderColor}
+        borderWidth={1}
+        h="60%"
+        w="30%"
+        p={10}
+        flexDir="column"
+        justifyContent="space-between"
+      >
+        <Flex flexDir="column" gap={4}>
+          <Heading as="h1">Join Learning U!</Heading>
+          <FormControl>
+            <FormLabel>Username or email</FormLabel>
+            <Input />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Password</FormLabel>
+            <Input />
+          </FormControl>
+        </Flex>
+        <Flex flexDir="column" gap={5}>
+          <Button variant="link">Already have an account? Login here</Button>
+          <Button>Register</Button>
+        </Flex>
+      </Flex>
+    </Center>
+  );
 };
 
 export default RegisterIndex;
