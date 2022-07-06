d54ab19023fb153327ceff4a6021ba76acbb6d44
Mauricio Munoz <maumd@fb.com>
2022-07-06 14:36:13 -0700

    Adding register endpoint

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -1,8 +1,25 @@
 import express from "express";
+import Parse from "parse/node";
+import { BadRequestError } from "../utils/errors";
+import type { IUser } from "../types/user";
 
 const auth = express.Router();
 
-auth.get("/login", (req, res) => {
-  res.send("Endpoint Working");
+auth.post("/register", async (req, res, next) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    next(new BadRequestError("Missing attributes"));
+    return;
+  }
+
+  const user = new Parse.User({ username, password, email });
+
+  try {
+    await user.signUp();
+    res.status(201).send(user);
+  } catch (error) {
+    next(new BadRequestError("Failed to created user: " + error));
+  }
 });
 export default auth;
