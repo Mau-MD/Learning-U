743b617a4a4e9edb73a6c0da456cf6f537f9fda9
Mauricio Munoz <maumd@fb.com>
2022-07-06 13:48:19 -0700

    Setting up parse

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import Parse from "parse/node";
 dotenv.config();
+
+Parse.initialize(process.env.PARSE_APP_ID, process.env.PARSE_JAVASCRIPT_KEY);
+Parse.serverURL = process.env.PARSE_SERVER_URL;
+
 const app = express();
 
 app.get("/", async (req, res) => {
-  res.status(200).send("Server Working!");
+  const testObject = new Parse.Object("test");
+  testObject.set("field1", "cool");
+  const result = await testObject.save();
+  res.send(result);
 });
 
 export default app;
