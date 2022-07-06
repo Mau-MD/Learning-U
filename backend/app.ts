df9f9b1bfdb3ed28e0a964ae41c54e88c741c0a5
Mauricio Munoz <maumd@fb.com>
2022-07-06 14:13:06 -0700

    Adding not found and error routes

--- backend/app.ts
+++ backend/app.ts
@@ -4,6 +4,7 @@ import Parse from "parse/node";
 
 // Import Routes
 import auth from "./routes/auth";
+import { NotFoundError } from "./utils/errors";
 
 dotenv.config();
 
@@ -19,6 +20,18 @@ app.get("/", async (req, res) => {
   testObject.set("field1", "cool");
   const result = await testObject.save();
   res.send(result);
+});
+
+app.use((req, res, next) => {
+  next(new NotFoundError());
+});
+
+app.use((error, req, res, next) => {
+  let { message, status } = error;
+  if (!status) status = 500;
+  if (!message) message = "Something went wrong in the application";
+
+  res.status(status).send({ status, message });
 });
 
 export default app;
