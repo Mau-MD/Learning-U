910480e28042f9aef7b6c426b1f845e94f62b89e
Mauricio Munoz <maumd@fb.com>
2022-07-06 14:40:57 -0700

    Adding login endpoint

--- backend/routes/auth.ts
+++ backend/routes/auth.ts
@@ -5,6 +5,22 @@ import type { IUser } from "../types/user";
 
 const auth = express.Router();
 
+auth.get("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    next(new BadRequestError("Missing attributes"));
+    return;
+  }
+
+  try {
+    const user = await Parse.User.logIn(username, password);
+    res.send({ user: user });
+  } catch (error) {
+    next(new BadRequestError("Login failed: " + error));
+  }
+});
+
 auth.post("/register", async (req, res, next) => {
   const { username, email, password } = req.body;
 
