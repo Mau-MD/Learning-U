8cb158086186806cd65824f08b5adbd7432106bc
Mauricio Munoz <maumd@fb.com>
2022-07-08 16:09:16 -0700

    Getting videos from youtube api

--- backend/routes/debug.ts
+++ backend/routes/debug.ts
@@ -1,5 +1,29 @@
-import express from "express";
+import express, { Request } from "express";
+import { getVideoDetailByIds, getVideosByQuery } from "../rating/youtube";
+import { BadRequestError } from "../utils/errors";
 
 const debug = express.Router();
+
+const VIDEOS_PER_QUERY = 100;
+
+debug.post("/", async (req, res, next) => {
+  const { query } = req.body;
+
+  if (!query) {
+    next(new BadRequestError("query is missing"));
+    return;
+  }
+
+  const videos = await getVideosByQuery(query, VIDEOS_PER_QUERY);
+
+  const ids = [];
+  for (const video of videos.data.items) {
+    ids.push(video.id.videoId);
+  }
+
+  const videosDetailed = await getVideoDetailByIds(ids, VIDEOS_PER_QUERY);
+
+  res.send(videosDetailed.data.items);
+});
 
 export default debug;
