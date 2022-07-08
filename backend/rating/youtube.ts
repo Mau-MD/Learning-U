a805ea40a47a510e838ea3375b55d5ee7eeea79e
Mauricio Munoz <maumd@fb.com>
2022-07-08 15:34:48 -0700

    Addubg getVideoDetailByIds

--- backend/rating/youtube.ts
+++ backend/rating/youtube.ts
@@ -4,3 +4,8 @@ export const getVideosByQuery = (query: string) => {
   const result = youtube.search.list({ q: query });
   return result;
 };
+
+export const getVideoDetailByIds = (ids: string[]) => {
+  const result = youtube.videos.list({ id: ids });
+  return result;
+};
