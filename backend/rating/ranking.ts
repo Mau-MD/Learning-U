0e2206cd2151546abb5364715cb6c14b58f3f0b4
Mauricio Munoz <maumd@fb.com>
2022-07-11 09:43:31 -0700

    Adding date score function

--- backend/rating/ranking.ts
+++ backend/rating/ranking.ts
@@ -1,5 +1,7 @@
 import type { youtube_v3 } from "googleapis/build/src/apis/youtube/v3";
 import { ExpressError } from "../utils/errors";
+import { parseISO, differenceInCalendarDays } from "date-fns";
+import { normalize } from "../utils/math";
 
 const WEIGHTS = {
   w1: 60,
@@ -14,6 +16,22 @@ export const getExternalRanking = (video: youtube_v3.Schema$Video) => {
   if (!ensureWeightsAreCorrect()) {
     new ExpressError("Ranking Weights are incorrect", 500);
   }
+
+  const dateScore = getDateScore(video.snippet.publishedAt);
+};
+
+const getDateScore = (publishedAt: string) => {
+  const publishedAtDate = parseISO(publishedAt);
+  const daysSincePublished = differenceInCalendarDays(
+    new Date(),
+    publishedAtDate
+  );
+  const yearsSincePublished = daysSincePublished / 365;
+
+  // Following the function in https://docs.google.com/document/d/1zxYRyytmbbvfAZkQc8dampD9Vhun0XQtWepKYxWUTKo
+  const dateScore = Math.max(-Math.pow(1.6, yearsSincePublished) + 11, 0);
+  const normalizedDateScore = normalize(dateScore, 0, 10);
+  return normalizedDateScore;
 };
 
 const ensureWeightsAreCorrect = () => {
