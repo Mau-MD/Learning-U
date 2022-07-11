52aec66be94569446fc49529a7035653ee6a6e71
Mauricio Munoz <maumd@fb.com>
2022-07-11 10:00:34 -0700

    Refactoring code so it works with an array of videos

--- backend/rating/ranking.ts
+++ backend/rating/ranking.ts
@@ -3,6 +3,10 @@ import { ExpressError } from "../utils/errors";
 import { parseISO, differenceInCalendarDays } from "date-fns";
 import { normalize } from "../utils/math";
 
+interface ExternalRankingParams {
+  wwwwww;
+}
+
 const WEIGHTS = {
   w1: 60,
   w2: 40,
@@ -12,27 +16,40 @@ const WEIGHTS = {
   w6: 80,
 };
 
-export const getExternalRanking = (video: youtube_v3.Schema$Video) => {
+export const getExternalRanking = (videos: youtube_v3.Schema$Video[]) => {
   if (!ensureWeightsAreCorrect()) {
     new ExpressError("Ranking Weights are incorrect", 500);
   }
+};
 
-  const daysSincePublished = getDaysSincePublished(video.snippet.publishedAt);
-  const yearsSincePublished = daysSincePublished / 365;
+const getRawExternalRanking = (videos: youtube_v3.Schema$Video[]) => {
+  videos.map((video) => {
+    const daysSincePublished = getDaysSincePublished(video.snippet.publishedAt);
+    const yearsSincePublished = daysSincePublished / 365;
 
-  const dateScore = getDateScore(yearsSincePublished);
+    const rawDateScore = getDateScore(yearsSincePublished);
+    const rawDateXViewsCore = getDateXViewsScore(
+      parseInt(video.statistics.viewCount),
+      yearsSincePublished
+    );
+    const rawDateXLikes = getDateXLikes(
+      parseInt(video.statistics.likeCount),
+      daysSincePublished
+    );
+  });
 };
 
 const getDateScore = (yearsSincePublished: number) => {
   // Following the function in https://docs.google.com/document/d/1zxYRyytmbbvfAZkQc8dampD9Vhun0XQtWepKYxWUTKo
-  const dateScore = Math.max(-Math.pow(1.6, yearsSincePublished) + 11, 0);
-  const normalizedDateScore = normalize(dateScore, 0, 10);
-  return normalizedDateScore;
+  return Math.max(-Math.pow(1.6, yearsSincePublished) + 11, 0);
 };
 
-const getDateXViewsScore = (views: number, yearsSincePublished: number) => {
-  const dateXViewsScore = views / yearsSincePublished;
-  return dateXViewsScore;
+const getDateXViewsScore = (views: number, daysSincePublished: number) => {
+  return views / daysSincePublished;
+};
+
+const getDateXLikes = (likes: number, daysSincePublished: number) => {
+  return likes / daysSincePublished;
 };
 
 const getDaysSincePublished = (publishedAt: string) => {
