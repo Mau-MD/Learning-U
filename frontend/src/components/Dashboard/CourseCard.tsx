39fe189d9bbf23f5586d54a8d597c657d453b212
Mauricio Munoz <maumd@fb.com>
2022-07-12 14:46:09 -0700

    Adding props to course card

--- frontend/src/components/Dashboard/CourseCard.tsx
+++ frontend/src/components/Dashboard/CourseCard.tsx
@@ -1,7 +1,6 @@
 import { ChevronDownIcon, HamburgerIcon } from "@chakra-ui/icons";
 import {
   Box,
-  Button,
   Flex,
   Heading,
   IconButton,
@@ -10,14 +9,27 @@ import {
   MenuButton,
   MenuItem,
   MenuList,
-  Text,
 } from "@chakra-ui/react";
 import React from "react";
 import useThemeColor from "../../hooks/useThemeColor";
 import Popover from "../Popover/Popover";
 import Tooltip from "../Popover/Tooltip";
 
-const CourseCard = () => {
+interface Props {
+  title: string;
+  src: string;
+  beginnerProgress: number;
+  intermediateProgress: number;
+  advancedProgress: number;
+}
+
+const CourseCard = ({
+  title,
+  src,
+  beginnerProgress,
+  intermediateProgress,
+  advancedProgress,
+}: Props) => {
   const { backgroundColor, borderColor } = useThemeColor();
   return (
     <Box
@@ -32,14 +44,14 @@ const CourseCard = () => {
         <Tooltip
           render={
             <Popover
-              courseName="React Course"
-              beginnerProgress={50}
-              intermediateProgress={20}
-              advancedProgress={0}
+              courseName={title}
+              beginnerProgress={beginnerProgress}
+              intermediateProgress={intermediateProgress}
+              advancedProgress={advancedProgress}
             />
           }
         >
-          <Image src="https://assets-global.website-files.com/61a0a53beeb118af7ddb4c55/61c0ba0267c18ebf1fd19b2f_maxresdefault-1-1-1024x576.jpeg"></Image>
+          <Image src={src}></Image>
         </Tooltip>
         <Flex
           padding={4}
@@ -47,7 +59,7 @@ const CourseCard = () => {
           alignItems={"center"}
         >
           <Heading as="h2" fontSize={"md"}>
-            React Course
+            {title}
           </Heading>
           <Menu>
             <MenuButton
--- frontend/src/components/Dashboard/DashboardIndex.tsx
+++ frontend/src/components/Dashboard/DashboardIndex.tsx
@@ -15,9 +15,27 @@ const DashboardIndex = () => {
           gap="1em"
           mt={10}
         >
-          <CourseCard />
-          <CourseCard />
-          <CourseCard />
+          <CourseCard
+            title="React Course"
+            src="https://assets-global.website-files.com/61a0a53beeb118af7ddb4c55/61c0ba0267c18ebf1fd19b2f_maxresdefault-1-1-1024x576.jpeg"
+            beginnerProgress={100}
+            intermediateProgress={40}
+            advancedProgress={10}
+          />
+          <CourseCard
+            title="Node JS Course"
+            src="https://assets-global.website-files.com/61a0a53beeb118af7ddb4c55/61c0ba0267c18ebf1fd19b2f_maxresdefault-1-1-1024x576.jpeg"
+            beginnerProgress={60}
+            intermediateProgress={10}
+            advancedProgress={0}
+          />
+          <CourseCard
+            title="Express Course"
+            src="https://assets-global.website-files.com/61a0a53beeb118af7ddb4c55/61c0ba0267c18ebf1fd19b2f_maxresdefault-1-1-1024x576.jpeg"
+            beginnerProgress={100}
+            intermediateProgress={100}
+            advancedProgress={90}
+          />
         </Grid>
       </Flex>
     </Container>
