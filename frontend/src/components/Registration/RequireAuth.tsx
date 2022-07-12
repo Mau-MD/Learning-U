f2e4187c57d1587548cde17152e0eddd4add8c09
Mauricio Munoz <maumd@fb.com>
2022-07-12 10:32:39 -0700

    Adding loading spinner

--- frontend/src/components/Registration/RequireAuth.tsx
+++ frontend/src/components/Registration/RequireAuth.tsx
@@ -1,3 +1,4 @@
+import { Box, Center, Spinner } from "@chakra-ui/react";
 import React from "react";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
 import { useSession } from "../../utils/auth";
@@ -5,15 +6,27 @@ import { useSession } from "../../utils/auth";
 interface Props {
   children: JSX.Element;
 }
+
 const RequireAuth = ({ children }: Props) => {
-  const { user } = useSession();
+  const { user, isFetching } = useSession();
   const location = useLocation();
 
-  if (!user) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
+  console.log(user, isFetching);
+  const handleRender = () => {
+    if (!isFetching)
+      return (
+        <Center>
+          <Center h="90vh">
+            <Spinner />
+          </Center>
+        </Center>
+      );
+    if (!user)
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    return children;
+  };
 
-  return children;
+  return <>{handleRender()}</>;
 };
 
 export default RequireAuth;
