81f4e157970b3ae9ae340d62bfa387eb6fe43ecc
Mauricio Munoz <maumd@fb.com>
2022-07-12 10:50:56 -0700

    Adding initial dashboard header

--- frontend/src/components/Dashboard/DashboardIndex.tsx
+++ frontend/src/components/Dashboard/DashboardIndex.tsx
@@ -1,7 +1,17 @@
+import { Button, Container, Flex, Heading, VStack } from "@chakra-ui/react";
 import React from "react";
 
 const DashboardIndex = () => {
-  return <div>DashboardIndex</div>;
+  return (
+    <Container maxW="container.xl">
+      <Flex flexDir={"column"} gap={4}>
+        <Heading as="h1" fontWeight="bold" fontSize="4xl" mt={10}>
+          Learning Dashboard
+        </Heading>
+        <Button w="fit-content">I want to learn something new...</Button>
+      </Flex>
+    </Container>
+  );
 };
 
 export default DashboardIndex;
