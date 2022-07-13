ea5de153fa749bb126440e114fa5f15c9ed1781c
Mauricio Munoz <maumd@fb.com>
2022-07-13 09:54:17 -0700

    adding new course route

--- backend/src/routes/course.ts
+++ backend/src/routes/course.ts
@@ -1,5 +1,19 @@
 import express from "express";
+import { createCourse } from "../course/course";
+import { ICourse } from "../types/course";
+import { BadRequestError } from "../utils/errors";
 
 const course = express.Router();
+
+course.post("/new", (req, res, next) => {
+  const { name } = req.body;
+
+  if (!name) {
+    next(new BadRequestError("Missing attributes"));
+    return;
+  }
+
+  const course = createCourse(name);
+});
 
 export default course;
