18afe2afaa3b53d3e91f193b79774b6c46da7428
Mauricio Munoz <maumd@fb.com>
2022-07-13 13:46:18 -0700

    Adding formik to new course form

--- frontend/src/components/NewCourse/NewCourseIndex.tsx
+++ frontend/src/components/NewCourse/NewCourseIndex.tsx
@@ -7,10 +7,19 @@ import {
   Heading,
   Input,
 } from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
 import React from "react";
 import Banner from "../Hub/Banner";
 
+interface LearnForm {
+  name: string;
+}
+
 const NewCourseIndex = () => {
+  const handleSubmit = (values: LearnForm) => {
+    console.log(values);
+  };
+
   return (
     <>
       <Banner src="https://images.unsplash.com/photo-1513258496099-48168024aec0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80" />
@@ -18,15 +27,21 @@ const NewCourseIndex = () => {
         <Heading as="h1" fontWeight="bold" fontSize="4xl" mt={10}>
           Let{"'"}s learn something new!
         </Heading>
-        <Flex flexDir="column" gap={10}>
-          <FormControl mt={10}>
-            <FormLabel>What do you want to learn?</FormLabel>
-            <Input />
-          </FormControl>
-          <Button w="100%" colorScheme="green">
-            Generate Course!
-          </Button>
-        </Flex>
+        <Formik initialValues={{ name: "" }} onSubmit={handleSubmit}>
+          {() => (
+            <Form>
+              <Flex flexDir="column" gap={10}>
+                <FormControl mt={10}>
+                  <FormLabel>What do you want to learn?</FormLabel>
+                  <Field as={Input} name="name" />
+                </FormControl>
+                <Button w="100%" colorScheme="green" type="submit">
+                  Generate Course!
+                </Button>
+              </Flex>
+            </Form>
+          )}
+        </Formik>
       </Container>
     </>
   );
