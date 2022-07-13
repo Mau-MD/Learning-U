6dcca6a37314feeeacd3bd4675c2463343f1b9e4
Mauricio Munoz <maumd@fb.com>
2022-07-13 11:06:06 -0700

    Adding resource type

---
+++ backend/src/types/resource.d.ts
@@ -0,0 +1,25 @@
+import { ICourse } from "./course";
+import Parse from "parse/node";
+
+export type IResource =
+  | {
+      type: "video";
+      status: "not started" | "in progress" | "completed";
+      level: 1 | 2 | 3;
+      feedback: number;
+      title: string;
+      description: string;
+      url: string;
+      thumbnail: string;
+      channel: string;
+      course: Parse.Object<Parse.Attributes>;
+    }
+  | {
+      type: "website";
+      status: "not started" | "in progress" | "completed";
+      level: 1 | 2 | 3;
+      feedback: number;
+      title: string;
+      url: string;
+      course: Parse.Object<Parse.Attributes>;
+    };
