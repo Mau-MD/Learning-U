f190240b1aaba95b6e20222644a2025d90821eef
Mauricio Munoz <maumd@fb.com>
2022-07-13 11:06:48 -0700

    saving resources for different difficulties

--- backend/src/routes/course.ts
+++ backend/src/routes/course.ts
@@ -1,11 +1,16 @@
 import express from "express";
-import { createCourse } from "../course/course";
+import {
+  createCourse,
+  createResource,
+  generateResources,
+  saveResources,
+} from "../course/course";
 import { ICourse } from "../types/course";
 import { BadRequestError } from "../utils/errors";
 
 const course = express.Router();
 
-course.post("/new", (req, res, next) => {
+course.post("/new", async (req, res, next) => {
   const { name } = req.body;
 
   if (!name) {
@@ -14,6 +19,20 @@ course.post("/new", (req, res, next) => {
   }
 
   const course = createCourse(name);
+  const { beginner, intermediate, advanced } = await generateResources(name);
+
+  try {
+    // Save course first
+    await course.save();
+
+    await saveResources(beginner, course, 1);
+    await saveResources(intermediate, course, 2);
+    await saveResources(advanced, course, 3);
+
+    res.status(201).send("Generated Course!");
+  } catch (error) {
+    next(new BadRequestError(error));
+  }
 });
 
 export default course;
