4c4b26cf610ffbbbd598e1d2278cb85bd32d970b
Mauricio Munoz <maumd@fb.com>
2022-07-15 11:09:15 -0700

    adding types for unsplash image links

--- frontend/src/types/course.d.ts
+++ frontend/src/types/course.d.ts
@@ -1,6 +1,9 @@
 import { DbObject } from "./global";
+import { IUnsplashLinks } from "./image";
+import { IUser } from "./user";
 
 export interface ICourse extends DbObject {
   name: string;
-  resources: any[];
+  user: IUser;
+  images: IUnsplashLinks[];
 }
