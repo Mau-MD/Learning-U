ce26e67cb318dad1c60965995b5cce023c7d514f
Mauricio Munoz <maumd@fb.com>
2022-07-15 09:51:07 -0700

    Adding fetching courses from database

--- frontend/src/components/Hub/HubIndex.tsx
+++ frontend/src/components/Hub/HubIndex.tsx
@@ -6,9 +6,37 @@ import {
   CircularProgressLabel,
 } from "@chakra-ui/react";
 import React from "react";
+import { useQuery } from "react-query";
+import { useParams, useSearchParams } from "react-router-dom";
+import { getConfig, useSession } from "../../utils/auth";
 import ResourceGroup from "./ResourceGroup";
+import axios from "axios";
+import { baseURL } from "../../utils/constants";
+import { IResource } from "../../types/resource";
 
 const HubIndex = () => {
+  const { isFetching, user } = useSession();
+  const [searchParams] = useSearchParams();
+
+  const { id } = useParams();
+  const difficulty = searchParams.get("difficulty");
+
+  const { data } = useQuery(
+    `hub-${id}-${difficulty}`,
+    async () => {
+      if (!user) throw new Error("User is not defined");
+
+      const res = await axios.get<IResource[]>(
+        `${baseURL}/resources/byCourse/${id}/${difficulty}`,
+        getConfig(user.sessionToken)
+      );
+      return res.data;
+    },
+    {
+      enabled: !isFetching && !!id && !!difficulty,
+    }
+  );
+
   return (
     <>
       <Box display="flex" alignItems="center" justifyContent="space-between">
@@ -16,16 +44,22 @@ const HubIndex = () => {
           <Heading as="h1" fontWeight="bold" fontSize="4xl" id="title">
             React
           </Heading>
-          <Badge>Beginner</Badge>
+          <Badge>{difficulty === "1" ? "Beginner" : "Advanced"}</Badge>
         </Box>
         <CircularProgress value={30} size={"60px"}>
           <CircularProgressLabel>30%</CircularProgressLabel>
         </CircularProgress>
       </Box>
       <Box display="flex" flexDirection="column" gap={10} my={10}>
-        <ResourceGroup title="🎥 Recommended Videos" kind="video" />
-        <ResourceGroup title="📚 Documentation to Read" kind="documentation" />
-        <ResourceGroup title="🎯 Projects to do" kind="video" />
+        {data && (
+          <>
+            <ResourceGroup
+              title="🎥 Recommended Videos"
+              kind="video"
+              data={data}
+            />
+          </>
+        )}
       </Box>
     </>
   );
