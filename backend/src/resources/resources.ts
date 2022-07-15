6229ab45da8124fdf3ed3a4e77c95fb2eda3d3d1
Mauricio Munoz <maumd@fb.com>
2022-07-15 09:51:33 -0700

    Adding function to get resources from course and difficulty

--- backend/src/resources/resources.ts
+++ backend/src/resources/resources.ts
@@ -25,3 +25,22 @@ export const getResourcesFromCourse = async (courseId: string) => {
   const resources = await query.findAll();
   return resources;
 };
+
+export const getResourcesFromCourseAndDifficulty = async (
+  courseId: string,
+  level: number
+) => {
+  const Resource = Parse.Object.extend("Resource");
+  const Course = Parse.Object.extend("Course");
+
+  const query = new Parse.Query(Resource);
+
+  const course = new Course();
+  course.id = courseId;
+
+  query.equalTo("course", course);
+  query.equalTo("level", level);
+
+  const resources = await query.findAll();
+  return resources;
+};
