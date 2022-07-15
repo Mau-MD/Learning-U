b1bdc136a16a0ce18b0c9f73ca8a9a1d55a9d03a
Mauricio Munoz <maumd@fb.com>
2022-07-15 11:37:25 -0700

    adding image fetching in layout component

--- frontend/src/components/Layouts/CourseLayout.tsx
+++ frontend/src/components/Layouts/CourseLayout.tsx
@@ -1,12 +1,41 @@
-import { Container, Heading } from "@chakra-ui/react";
+import { Container, Heading, Skeleton } from "@chakra-ui/react";
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useParams } from "react-router-dom";
+import { getConfig, useSession } from "../../utils/auth";
 import Banner from "../Hub/Banner";
+import { useQuery } from "react-query";
+import { baseURL } from "../../utils/constants";
+import axios from "axios";
+import { ICourse } from "../../types/course";
 
 const CourseLayout = () => {
+  const { id } = useParams();
+  const { user, isFetching } = useSession();
+
+  const { data, isLoading } = useQuery(
+    `${id}`,
+    async () => {
+      if (!user) throw new Error("User is not defiend");
+
+      const res = await axios.get<ICourse>(
+        `${baseURL}/course/${id}`,
+        getConfig(user.sessionToken)
+      );
+      return res.data;
+    },
+    { enabled: !!user && !!id }
+  );
+
   return (
     <>
-      <Banner src="https://miro.medium.com/max/1838/1*uE1kDeKaMnZaZLLJzHm0QA.png" />
+      {isLoading ? (
+        <Skeleton h={"40vh"} />
+      ) : (
+        <Banner
+          src={data?.images[0].regular || ""}
+          fallback={data?.images[0].thumb}
+        />
+      )}
       <Container maxW={"container.xl"} mt="2rem">
         <Outlet />
       </Container>
