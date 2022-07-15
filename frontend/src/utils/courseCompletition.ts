b21da8d675a9a38c7f52dd9773b1cd6e6ca90679
Mauricio Munoz <maumd@fb.com>
2022-07-15 15:30:11 -0700

    changing courseCompletition formula so it takes inProgress into account

--- frontend/src/utils/courseCompletition.ts
+++ frontend/src/utils/courseCompletition.ts
@@ -2,8 +2,12 @@ import { IResource } from "../types/resource";
 
 export const calculateCourseCompletition = (resources: IResource[]) => {
   let totalCompleted = 0;
+  let totalInProgress = 0;
+
   for (const resource of resources) {
     if (resource.status === "completed") totalCompleted++;
+    if (resource.status === "in progress") totalInProgress++;
   }
-  return totalCompleted / resources.length;
+
+  return ((totalCompleted + 0.5 * totalInProgress) / resources.length) * 100;
 };
