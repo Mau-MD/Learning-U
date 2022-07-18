ba3c2f914d9cad82bef710d962a4f315923f2e00
Mauricio Munoz <maumd@fb.com>
2022-07-18 11:27:42 -0700

    Adding create post route

--- backend/src/routes/post.ts
+++ backend/src/routes/post.ts
@@ -1,5 +1,30 @@
 import express from "express";
+import { getAuthUser } from "../middleware/getAuthUser";
+import { createPost } from "../post/post";
+import { RequestWUser } from "../types/user";
+import { BadRequestError } from "../utils/errors";
 
 const post = express.Router();
+
+post.use(getAuthUser);
+
+post.post("/", async (req: RequestWUser, res, next) => {
+  const { user } = req;
+  const { content, courseId } = req.body;
+
+  if (!content) {
+    next(new BadRequestError("Missing parameters"));
+    return;
+  }
+
+  try {
+    const post = createPost(content, user, courseId);
+    const result = await post.save();
+    res.status(201).send(result);
+  } catch (err) {
+    next(new BadRequestError(err));
+    return;
+  }
+});
 
 export default post;
