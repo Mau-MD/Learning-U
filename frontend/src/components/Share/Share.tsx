7f8aff97bf8f14ea8ceb54d92f55b3e7eade8167
Mauricio Munoz <maumd@fb.com>
2022-07-18 13:46:11 -0700

    Adding clipboard hook to the modal

--- frontend/src/components/Share/Share.tsx
+++ frontend/src/components/Share/Share.tsx
@@ -1,12 +1,16 @@
 import {
+  Button,
+  Flex,
+  Input,
   Modal,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  useClipboard,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   isOpen: boolean;
@@ -14,13 +18,22 @@ interface Props {
 }
 
 const Share = ({ isOpen, onClose }: Props) => {
+  const [value, setValue] = useState("CODE");
+
+  const { onCopy, hasCopied } = useClipboard(value);
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>Share this course to your friends!</ModalHeader>
         <ModalCloseButton />
-        <ModalBody>hola</ModalBody>
+        <ModalBody pb={4}>
+          <Flex gap={2}>
+            <Input value={value} isReadOnly />
+            <Button onClick={onCopy}>{hasCopied ? "Copied" : "Copy"}</Button>
+          </Flex>
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
