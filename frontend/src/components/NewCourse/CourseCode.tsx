5a89fd390a7efbbc55f6360174afe80ee2d2565b
Mauricio Munoz <maumd@fb.com>
2022-07-19 09:55:42 -0700

    Adding name form input inside Course Code Form

--- frontend/src/components/NewCourse/CourseCode.tsx
+++ frontend/src/components/NewCourse/CourseCode.tsx
@@ -7,20 +7,24 @@ import {
   Button,
   Box,
 } from "@chakra-ui/react";
+import axios from "axios";
 import { Field, Form, Formik } from "formik";
 import React from "react";
+import { useMutation } from "react-query";
+import { ICourse } from "../../types/course";
+import { getConfig, useSession } from "../../utils/auth";
+import { baseURL } from "../../utils/constants";
 
 interface CodeForm {
   code: string;
+  name: string;
 }
 const CourseCode = () => {
-  const handleOnSubmit = (values: CodeForm) => {
-    console.log(values);
-  };
+  const handleOnSubmit = (values: CodeForm) => {};
 
   return (
     <Box mb={20}>
-      <Formik initialValues={{ code: "" }} onSubmit={handleOnSubmit}>
+      <Formik initialValues={{ code: "", name: "" }} onSubmit={handleOnSubmit}>
         {({ touched, errors }) => (
           <Form>
             <FormControl mt={10} isInvalid={touched.code && !!errors.code}>
@@ -31,7 +35,12 @@ const CourseCode = () => {
               </FormHelperText>
               <FormErrorMessage>{errors.code}</FormErrorMessage>
             </FormControl>
-            <Button colorScheme="blue" w="100%" mt={5}>
+            <FormControl mt={5} isInvalid={touched.name && !!errors.name}>
+              <FormLabel>How do you want to call your course?</FormLabel>
+              <Field as={Input} name="name" />
+              <FormErrorMessage>{errors.name}</FormErrorMessage>
+            </FormControl>
+            <Button colorScheme="blue" w="100%" mt={5} type="submit">
               Clone Course
             </Button>
           </Form>
