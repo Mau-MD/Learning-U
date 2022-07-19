cd1a22f942ca93bc01f54b1e29ac767afb5acc75
Mauricio Munoz <maumd@fb.com>
2022-07-19 13:44:02 -0700

    Adding grid to display multiple video cards when user want's to delete a course

--- frontend/src/components/DeleteCourse/DeleteCourse.tsx
+++ frontend/src/components/DeleteCourse/DeleteCourse.tsx
@@ -5,8 +5,12 @@ import {
   ModalHeader,
   ModalCloseButton,
   ModalBody,
+  Text,
+  Grid,
 } from "@chakra-ui/react";
 import React from "react";
+import VideoCard from "../Hub/VideoCard";
+import VideoCardToDelete from "./VideoCardToDelete";
 
 interface Props {
   isOpen: boolean;
@@ -15,12 +19,17 @@ interface Props {
 
 const DeleteCourse = ({ isOpen, onClose }: Props) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} size="4xl">
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Share this course to your friends!</ModalHeader>
+        <ModalHeader>Are you sure you want to delete this course?</ModalHeader>
         <ModalCloseButton />
-        <ModalBody pb={7}></ModalBody>
+        <ModalBody pb={7}>
+          <Text mb={2}>Select which tutorials you didn{"'"}t like?</Text>
+          <Grid gridTemplateColumns="repeat(2, 1fr)" gap={5}>
+            <VideoCardToDelete title="Video 1" src="" />
+          </Grid>
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
