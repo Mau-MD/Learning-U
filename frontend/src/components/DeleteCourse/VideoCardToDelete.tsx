755c8c5a40ae518a4e610af967571d84fdd4d5aa
Mauricio Munoz <maumd@fb.com>
2022-07-19 14:09:38 -0700

    adding onClick and objectId props so an user can select which videos it didn't like when deleting a course

--- frontend/src/components/DeleteCourse/VideoCardToDelete.tsx
+++ frontend/src/components/DeleteCourse/VideoCardToDelete.tsx
@@ -6,17 +6,27 @@ import useThemeColor from "../../hooks/useThemeColor";
 interface Props {
   src: string;
   title: string;
+  active: boolean;
+  objectId: string;
+  onClick: (objectId: string) => void;
 }
-const VideoCardToDelete = ({ src, title }: Props) => {
+const VideoCardToDelete = ({
+  src,
+  title,
+  objectId,
+  onClick,
+  active,
+}: Props) => {
   const { backgroundColor, borderColor } = useThemeColor();
 
   return (
     <Box
       bg={backgroundColor}
-      borderColor={borderColor}
-      borderWidth={1}
+      borderColor={active ? "blue.500" : borderColor}
+      borderWidth={active ? 5 : 1}
       borderRadius={4}
       transition={"all 0.3s"}
+      onClick={() => onClick(objectId)}
     >
       <Box w="100%" h="220px">
         <Image src={src} w="full" h="full" objectFit="cover" cursor="pointer" />
