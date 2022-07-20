3311ded13896ca2785c274b94c8f9b676882dda6
Mauricio Munoz <maumd@fb.com>
2022-07-20 15:19:56 -0700

    Adding helper functions to update a feedback object, and if it doesn't exist, it creates one.

---
+++ backend/src/feedback/feedback.ts
@@ -0,0 +1,30 @@
+import Parse from "parse/node";
+
+export const createFeedback = (videoId: string, feedback: number) => {
+  const Feedback = new Parse.Object("Feedback");
+  Feedback.set("videoId", videoId);
+  Feedback.set("feedback", feedback);
+  return Feedback;
+};
+
+export const updateFeedback = async (
+  videoId: string,
+  amountToIncrement: number
+) => {
+  const feedback = await getFeedbackByVideoId(videoId);
+
+  if (!feedback || feedback.length === 0) {
+    return createFeedback(videoId, amountToIncrement);
+  }
+
+  return feedback[0].increment("feedback", amountToIncrement);
+};
+
+export const getFeedbackByVideoId = async (videoId: string) => {
+  const Feedback = Parse.Object.extend("Feedback");
+  const query = new Parse.Query(Feedback);
+
+  query.equalTo("videoId", videoId);
+  const feedback = await query.find();
+  return feedback;
+};
