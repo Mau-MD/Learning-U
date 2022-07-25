11ed56fd7942f7ed51b062baf5807b7467a71708
Mauricio Munoz <maumd@fb.com>
2022-07-25 10:29:24 -0700

    Displaying in the frontend post fetched from the backend

--- frontend/src/components/Feed/FeedIndex.tsx
+++ frontend/src/components/Feed/FeedIndex.tsx
@@ -1,7 +1,41 @@
+import { Box, Container, VStack } from "@chakra-ui/react";
+import axios from "axios";
 import React from "react";
+import { useQuery } from "react-query";
+import { IPost } from "../../types/post";
+import { getConfig, useSession } from "../../utils/auth";
+import { baseURL } from "../../utils/constants";
+import FeedCard from "./FeedCard";
 
 const FeedIndex = () => {
-  return <div>FeedIndex</div>;
+  const { user } = useSession();
+
+  const { data: posts } = useQuery(
+    "posts",
+    async () => {
+      if (!user) {
+        throw new Error("User is not defined");
+      }
+
+      const res = await axios.get<IPost[]>(
+        `${baseURL}/post/me`,
+        getConfig(user?.sessionToken)
+      );
+      return res.data;
+    },
+    { enabled: !!user }
+  );
+
+  return (
+    <Container maxW="container.xl">
+      <VStack mt={10} gap={19}>
+        {posts &&
+          posts.map((post) => (
+            <FeedCard key={post.objectId} content={post.content} />
+          ))}
+      </VStack>
+    </Container>
+  );
 };
 
 export default FeedIndex;
