7e14e22a06f61c81595a4b2fecfe8118c8a8d9b5
Mauricio Munoz <maumd@fb.com>
2022-07-26 14:08:19 -0700

    Adding formik to folow someone form so we can handle user form with eas

--- frontend/src/components/Following/FollowForm.tsx
+++ frontend/src/components/Following/FollowForm.tsx
@@ -5,17 +5,34 @@ import {
   Input,
   Button,
 } from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
 import React from "react";
 
+interface FollowForm {
+  username: string;
+}
+
 const FollowForm = () => {
+  const handleSubmit = (values: FollowForm) => {
+    console.log(values);
+  };
+
   return (
-    <VStack mt={4}>
-      <FormControl>
-        <FormLabel>Follow someone</FormLabel>
-        <Input />
-      </FormControl>
-      <Button w={"100%"}>Follow!</Button>
-    </VStack>
+    <Formik initialValues={{ username: "" }} onSubmit={handleSubmit}>
+      {() => (
+        <Form>
+          <VStack mt={4}>
+            <FormControl>
+              <FormLabel>Follow someone</FormLabel>
+              <Field as={Input} name="username" />
+            </FormControl>
+            <Button type="submit" w={"100%"}>
+              Follow!
+            </Button>
+          </VStack>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
