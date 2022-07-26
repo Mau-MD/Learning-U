1ceed60b782f1afa37f9071cf1b4ab790265f6ae
Mauricio Munoz <maumd@fb.com>
2022-07-26 15:35:55 -0700

    Adding mutation to update status in the backend

--- frontend/src/components/Following/SetStatusForm.tsx
+++ frontend/src/components/Following/SetStatusForm.tsx
@@ -4,11 +4,17 @@ import {
   FormErrorMessage,
   FormLabel,
   Input,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
+import axios, { AxiosError } from "axios";
 import { Field, Form, Formik } from "formik";
 import React from "react";
+import { useMutation } from "react-query";
 import * as Yup from "yup";
+import { ErrorType } from "../../types/requests";
+import { getConfig, useSession } from "../../utils/auth";
+import { baseURL } from "../../utils/constants";
 
 interface StatusForm {
   status: string;
@@ -19,9 +25,43 @@ const schema = Yup.object({
 });
 
 const SetStatusForm = () => {
+  const toast = useToast();
+  const { user } = useSession();
+
   const handleFormSubmit = (values: StatusForm) => {
-    console.log(values);
+    setStatus.mutate(values.status);
   };
+
+  const setStatus = useMutation(
+    async (status: string) => {
+      if (!user) throw new Error("User is not defined");
+
+      const res = await axios.post(
+        `${baseURL}/follow/status`,
+        { status },
+        getConfig(user?.sessionToken)
+      );
+      return res.data;
+    },
+    {
+      onSuccess: () => {
+        toast({
+          title: "Success",
+          description: `Your status has been updated`,
+          status: "success",
+        });
+      },
+      onError: (error: AxiosError<ErrorType>) => {
+        toast({
+          title: "An error ocurred",
+          description: error.response?.data.message,
+          status: "error",
+          duration: 5000,
+          isClosable: true,
+        });
+      },
+    }
+  );
 
   return (
     <Formik
