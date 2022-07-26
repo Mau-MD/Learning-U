4ac754f43d87c6c0ad6dca9c4c72990e87435388
Mauricio Munoz <maumd@fb.com>
2022-07-26 15:30:42 -0700

    Adding yup to status form to handle errors when required

--- frontend/src/components/Following/SetStatusForm.tsx
+++ frontend/src/components/Following/SetStatusForm.tsx
@@ -1,16 +1,22 @@
 import {
   Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   VStack,
 } from "@chakra-ui/react";
 import { Field, Form, Formik } from "formik";
 import React from "react";
+import * as Yup from "yup";
 
 interface StatusForm {
   status: string;
 }
+
+const schema = Yup.object({
+  status: Yup.string().required("Status is required"),
+});
 
 const SetStatusForm = () => {
   const handleFormSubmit = (values: StatusForm) => {
@@ -18,13 +24,18 @@ const SetStatusForm = () => {
   };
 
   return (
-    <Formik initialValues={{ status: "" }} onSubmit={handleFormSubmit}>
-      {() => (
+    <Formik
+      initialValues={{ status: "" }}
+      onSubmit={handleFormSubmit}
+      validationSchema={schema}
+    >
+      {({ errors, touched }) => (
         <Form>
           <VStack mt={4}>
-            <FormControl>
+            <FormControl isInvalid={touched.status && !!errors.status}>
               <FormLabel>Status</FormLabel>
               <Field as={Input} name="status" />
+              <FormErrorMessage>{errors.status}</FormErrorMessage>
             </FormControl>
             <Button type="submit" w={"100%"}>
               Update Status
