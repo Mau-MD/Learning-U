0e0f75e1f936b56d17cd6dd2298b9b282c3f04e0
Mauricio Munoz <maumd@fb.com>
2022-07-26 14:03:33 -0700

    Adding tab functionality so it renders both status and follow form conditionally

--- frontend/src/components/Following/FollowingIndex.tsx
+++ frontend/src/components/Following/FollowingIndex.tsx
@@ -1,11 +1,36 @@
-import { Box, Heading, VStack } from "@chakra-ui/react";
-import React from "react";
+import {
+  Box,
+  Button,
+  Heading,
+  HStack,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  VStack,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
 import useThemeColor from "../../hooks/useThemeColor";
+import FollowForm from "./FollowForm";
 import FollwingCard from "./FollwingCard";
 import SetStatusForm from "./SetStatusForm";
 
 const FollowingIndex = () => {
   const { backgroundColor, borderColor } = useThemeColor();
+
+  const [tabSelected, setTabSelected] = useState<"none" | "status" | "follow">(
+    "none"
+  );
+
+  const handleTabChange = (tab: "none" | "status" | "follow") => {
+    if (tabSelected === tab) {
+      setTabSelected("none");
+      return;
+    }
+    setTabSelected(tab);
+  };
+
   return (
     <Box
       w="30%"
@@ -20,7 +45,24 @@ const FollowingIndex = () => {
       <Heading size={"md"} as={"h2"}>
         Following
       </Heading>
-      <SetStatusForm />
+      <HStack w={"100%"} mt={5}>
+        <Button
+          w={"100%"}
+          onClick={() => handleTabChange("status")}
+          isActive={tabSelected === "status"}
+        >
+          Status
+        </Button>
+        <Button
+          w={"100%"}
+          onClick={() => handleTabChange("follow")}
+          isActive={tabSelected === "follow"}
+        >
+          Follow
+        </Button>
+      </HStack>
+      {tabSelected === "status" && <SetStatusForm />}
+      {tabSelected === "follow" && <FollowForm />}
       <VStack mt={5}>
         <FollwingCard username="clanie1" status="cool status" />
         <FollwingCard username="clinigambit" status="not cool" />
