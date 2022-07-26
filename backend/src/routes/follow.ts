76c0ccf79e6a128edd415b956052078f7331f655
Mauricio Munoz <maumd@fb.com>
2022-07-26 13:30:57 -0700

    adding status route to set a status

--- backend/src/routes/follow.ts
+++ backend/src/routes/follow.ts
@@ -4,8 +4,10 @@ import {
   getFollowers,
   getFollowersIds,
 } from "../following/following";
+import { setStatus } from "../following/status";
 import { getAuthUser } from "../middleware/getAuthUser";
 import { RequestWUser } from "../types/user";
+import { BadRequestError } from "../utils/errors";
 
 const follow = express.Router();
 
@@ -17,12 +19,24 @@ follow.get("/getFollowers", async (req: RequestWUser, res, next) => {
   res.send(await getFollowersIds(user));
 });
 
-follow.post("/follow/:targetId", async (req: RequestWUser, res, next) => {
+follow.post("/followUser/:targetId", async (req: RequestWUser, res, next) => {
   const { user } = req;
   const { targetId } = req.params;
 
   const Follower = followUser(user, targetId);
   res.send(await Follower.save());
+});
+
+follow.post("/status", async (req: RequestWUser, res, next) => {
+  const { user } = req;
+  const { status } = req.body;
+
+  if (!status) {
+    next(new BadRequestError("Missing params"));
+    return;
+  }
+
+  res.send(await setStatus(user, status));
 });
 
 export default follow;
