8bfa30dc4e661a4c21a09ed8ac3a326cea8cb53f
Mauricio Munoz <maumd@fb.com>
2022-07-26 10:58:50 -0700

    adding follow router inside app.ts

--- backend/src/routes/follow.ts
+++ backend/src/routes/follow.ts
@@ -1,5 +1,15 @@
 import express from "express";
+import { followUser } from "../following/following";
+import { RequestWUser } from "../types/user";
 
 const follow = express.Router();
+
+follow.post("/:targetId", async (req: RequestWUser, res, next) => {
+  const { user } = req;
+  const { targetId } = req.params;
+
+  const Follower = followUser(user, targetId);
+  res.send(await Follower.save());
+});
 
 export default follow;
