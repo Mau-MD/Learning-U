ea08be98d8f3560d0738f6e295a933ccc723afd1
Mauricio Munoz <maumd@fb.com>
2022-07-26 14:10:04 -0700

    Adding form validation with yup

--- frontend/src/components/Following/FollowForm.tsx
+++ frontend/src/components/Following/FollowForm.tsx
@@ -4,13 +4,19 @@ import {
   FormLabel,
   Input,
   Button,
+  FormErrorMessage,
 } from "@chakra-ui/react";
 import { Field, Form, Formik } from "formik";
 import React from "react";
+import * as Yup from "yup";
 
 interface FollowForm {
   username: string;
 }
+
+const schema = Yup.object({
+  username: Yup.string().required("Username is required"),
+});
 
 const FollowForm = () => {
   const handleSubmit = (values: FollowForm) => {
@@ -18,13 +24,18 @@ const FollowForm = () => {
   };
 
   return (
-    <Formik initialValues={{ username: "" }} onSubmit={handleSubmit}>
-      {() => (
+    <Formik
+      initialValues={{ username: "" }}
+      onSubmit={handleSubmit}
+      validationSchema={schema}
+    >
+      {({ touched, errors }) => (
         <Form>
           <VStack mt={4}>
-            <FormControl>
+            <FormControl isInvalid={touched.username && !!errors.username}>
               <FormLabel>Follow someone</FormLabel>
               <Field as={Input} name="username" />
+              <FormErrorMessage>{errors.username}</FormErrorMessage>
             </FormControl>
             <Button type="submit" w={"100%"}>
               Follow!
