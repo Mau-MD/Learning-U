6ca352a188738da78ac26cd23f0295ca07163942
Mauricio Munoz <maumd@fb.com>
2022-07-26 13:30:39 -0700

    Adding status helper funcitons that interact with the status table. It creates a status if it doesn't exist yet and updates it if it exists

---
+++ backend/src/following/status.ts
@@ -0,0 +1,32 @@
+import Parse from "parse/node";
+
+export const setStatus = async (
+  user: Parse.User<Parse.Attributes>,
+  status: string
+) => {
+  const userStatus = await findStatusByUser(user);
+
+  if (userStatus.length === 0) {
+    return await createStatus(user, status);
+  }
+
+  userStatus[0].set("status", status);
+  return await userStatus[0].save();
+};
+
+export const findStatusByUser = async (user: Parse.User<Parse.Attributes>) => {
+  const Status = Parse.Object.extend("Status");
+  const query = new Parse.Query(Status);
+  query.equalTo("user", user);
+  return await query.find();
+};
+
+export const createStatus = async (
+  user: Parse.User<Parse.Attributes>,
+  status: string
+) => {
+  const Status = new Parse.Object("Status");
+  Status.set("status", status);
+  Status.set("user", user);
+  return await Status.save();
+};
