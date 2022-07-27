acc67a353cbbd45f991c5791932605215af1f756
Mauricio Munoz <maumd@fb.com>
2022-07-27 11:02:54 -0700

    Adding mutation to stop following a particular user

--- frontend/src/components/Following/FollowingCard.tsx
+++ frontend/src/components/Following/FollowingCard.tsx
@@ -6,11 +6,17 @@ import {
   HStack,
   IconButton,
   Text,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
+import axios, { AxiosError } from "axios";
 import React from "react";
 import { BiTrash } from "react-icons/bi";
+import { useMutation, useQueryClient } from "react-query";
 import useThemeColor from "../../hooks/useThemeColor";
+import { ErrorType } from "../../types/requests";
+import { getConfig, useSession } from "../../utils/auth";
+import { baseURL } from "../../utils/constants";
 
 interface Props {
   username: string;
@@ -20,6 +26,42 @@ interface Props {
 
 const FollowingCard = ({ username, userId, status }: Props) => {
   const { borderColor, invertedBackgroundColor } = useThemeColor();
+
+  const queryClient = useQueryClient();
+  const toast = useToast();
+  const { user } = useSession();
+
+  const unfollowUser = useMutation(
+    async (userId: string) => {
+      if (!user) {
+        throw new Error("User is not defined");
+      }
+      const res = await axios.delete(`${baseURL}/follow/${userId}`, {
+        ...getConfig(user.sessionToken),
+      });
+      return res.data;
+    },
+    {
+      onSuccess: () => {
+        toast({
+          status: "success",
+          title: "Success",
+          description: `You stopped following ${username}`,
+          isClosable: true,
+        });
+        queryClient.invalidateQueries("following");
+      },
+      onError: (error: AxiosError<ErrorType>) => {
+        toast({
+          status: "error",
+          title: "Error deleting course",
+          description: error.response?.data.message,
+          isClosable: true,
+        });
+      },
+    }
+  );
+
   return (
     <HStack
       w="100%"
@@ -39,6 +81,8 @@ const FollowingCard = ({ username, userId, status }: Props) => {
         aria-label="Unfollow user"
         size="sm"
         colorScheme="red"
+        isLoading={unfollowUser.isLoading}
+        onClick={() => unfollowUser.mutate(userId)}
       />
     </HStack>
   );
