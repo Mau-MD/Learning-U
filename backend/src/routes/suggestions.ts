3a1cc69040070dbdf361072e0df3ce3966859c92
Mauricio Munoz <maumd@fb.com>
2022-07-28 15:23:09 -0700

    addoing route to get suggestions from a user

--- backend/src/routes/suggestions.ts
+++ backend/src/routes/suggestions.ts
@@ -1,5 +1,17 @@
 import express from "express";
+import { getAuthUser } from "../middleware/getAuthUser";
+import { getSuggestions } from "../suggestions/suggestions";
+import { RequestWUser } from "../types/user";
 
 const suggestions = express.Router();
+
+suggestions.use(getAuthUser);
+
+suggestions.get("/me", async (req: RequestWUser, res, next) => {
+  const { user } = req;
+
+  const suggestions = await getSuggestions(user);
+  res.send(suggestions);
+});
 
 export default suggestions;
