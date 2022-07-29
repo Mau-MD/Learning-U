02fc93b003f236f70f103acbd74aa3ccfff87c05
Mauricio Munoz <maumd@fb.com>
2022-07-29 10:57:18 -0700

    adding route to get user by id

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,9 +1,16 @@
 import express from "express";
 import { getAuthUser } from "../middleware/getAuthUser";
 import { RequestWUser } from "../types/user";
+import { getUserById } from "../user/user";
 
 const user = express.Router();
 
 user.use(getAuthUser);
+
+user.get("/by/:id", async (req: RequestWUser, res, next) => {
+  const { id } = req.params;
+  const user = await getUserById(id);
+  res.send(user);
+});
 
 export default user;
