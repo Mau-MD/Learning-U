4783ac58046905212c743cca4349877a90ecca35
Mauricio Munoz <maumd@fb.com>
2022-08-01 10:28:23 -0700

    Adding basic ui to show inside the feautred index

--- frontend/src/components/Featured/FeaturedIndex.tsx
+++ frontend/src/components/Featured/FeaturedIndex.tsx
@@ -1,7 +1,47 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Grid,
+  Heading,
+  HStack,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
+import CourseCard from "../Dashboard/CourseCard";
+import DashboardIndex, { FALLBACK_IMG } from "../Dashboard/DashboardIndex";
+import LoadingCard from "../Loading/LoadingCard";
 
 const FeaturedIndex = () => {
-  return <div>FeaturedIndex</div>;
+  return (
+    <Container maxW={"container.xl"}>
+      <Flex flexDir={"column"} gap={4}>
+        <Heading as="h1" fontWeight="bold" fontSize="4xl" mt={10}>
+          Featured Courses
+        </Heading>
+        <Text>Courses submitted by the community </Text>
+        <HStack>
+          <Button>Submit an existing course</Button>
+          <Button>Create a new course</Button>
+        </HStack>
+        <Grid
+          templateColumns={["1fr", "1fr", "repeat(2, 1fr)", "repeat(3, 1fr)"]}
+          gap="1em"
+          mt={10}
+          mb={"200px"}
+        >
+          <CourseCard
+            link={""}
+            title={"hola"}
+            src={FALLBACK_IMG}
+            createdAt={new Date().toString()}
+            cloneButton
+          />
+        </Grid>
+      </Flex>
+    </Container>
+  );
 };
 
 export default FeaturedIndex;
