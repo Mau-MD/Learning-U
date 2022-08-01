35150db2d5e87ec4dfcadc465217db97915e2813
Mauricio Munoz <maumd@fb.com>
2022-08-01 11:44:02 -0700

    Copying infinite scroll logic from dashboard index. adding mutation to fetch featured courses and display them on the frontend

--- frontend/src/components/Featured/FeaturedIndex.tsx
+++ frontend/src/components/Featured/FeaturedIndex.tsx
@@ -8,13 +8,72 @@ import {
   HStack,
   Text,
 } from "@chakra-ui/react";
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useInfiniteQuery } from "react-query";
+import { createSearchParams } from "react-router-dom";
+import { ICourse } from "../../types/course";
+import { getConfig, useSession } from "../../utils/auth";
+import { baseURL } from "../../utils/constants";
+import { debounce } from "../../utils/debounce";
 import CourseCard from "../Dashboard/CourseCard";
 import DashboardIndex, { FALLBACK_IMG } from "../Dashboard/DashboardIndex";
 import Banner from "../Hub/Banner";
 import LoadingCard from "../Loading/LoadingCard";
 
+const COURSES_PER_FETCH = 6;
+
 const FeaturedIndex = () => {
+  const { user } = useSession();
+
+  const {
+    data: courses,
+    fetchNextPage,
+    isFetching,
+    isLoading,
+  } = useInfiniteQuery(
+    "featured",
+    async ({ pageParam = 0 }) => {
+      if (!user) {
+        throw new Error("User is not defined");
+      }
+      const res = await axios.get<ICourse[]>(
+        `${baseURL}/course/featured/get?${createSearchParams({
+          limit: `${COURSES_PER_FETCH}`,
+          skip: `${pageParam}`,
+          query: "",
+        })}`,
+        getConfig(user?.sessionToken)
+      );
+
+      return { data: res.data, currentPage: pageParam };
+    },
+    {
+      getNextPageParam: (params) => {
+        if (params.data.length < COURSES_PER_FETCH) return undefined;
+        return params.currentPage + params.data.length;
+      },
+      enabled: !!user,
+    }
+  );
+
+  useEffect(() => {
+    window.addEventListener("scroll", handlePageBottom);
+
+    return () => {
+      window.removeEventListener("scroll", handlePageBottom);
+    };
+  }, []);
+
+  const handlePageBottom = debounce(() => {
+    if (
+      window.innerHeight + window.scrollY >= document.body.offsetHeight &&
+      !isFetching
+    ) {
+      fetchNextPage();
+    }
+  }, 500);
+
   return (
     <>
       <Banner src="https://images.unsplash.com/photo-1514820720301-4c4790309f46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80" />
@@ -34,13 +93,41 @@ const FeaturedIndex = () => {
             mt={10}
             mb={"200px"}
           >
-            <CourseCard
-              link={""}
-              title={"hola"}
-              src={FALLBACK_IMG}
-              createdAt={new Date().toString()}
-              cloneButton
-            />
+            {isLoading ? (
+              <>
+                <LoadingCard />
+                <LoadingCard />
+                <LoadingCard />
+              </>
+            ) : (
+              courses &&
+              courses.pages.map((page) =>
+                page.data.map((course) => (
+                  <CourseCard
+                    key={course.objectId}
+                    link={course.objectId}
+                    title={course.name}
+                    src={
+                      course.images
+                        ? course.images[2]
+                          ? course.images[2].regular
+                          : FALLBACK_IMG
+                        : FALLBACK_IMG
+                    }
+                    cloneButton
+                    createdAt={course.createdAt}
+                    liked={false}
+                  />
+                ))
+              )
+            )}
+            {isFetching && (
+              <>
+                <LoadingCard />
+                <LoadingCard />
+                <LoadingCard />
+              </>
+            )}
           </Grid>
         </Flex>
       </Container>
