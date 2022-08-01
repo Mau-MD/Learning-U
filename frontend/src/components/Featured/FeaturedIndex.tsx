347b9a6b726ceabc0e7a0c9424440e5a1c150f2e
Mauricio Munoz <maumd@fb.com>
2022-08-01 10:32:08 -0700

    adding banner to featured page

--- frontend/src/components/Featured/FeaturedIndex.tsx
+++ frontend/src/components/Featured/FeaturedIndex.tsx
@@ -11,36 +11,40 @@ import {
 import React from "react";
 import CourseCard from "../Dashboard/CourseCard";
 import DashboardIndex, { FALLBACK_IMG } from "../Dashboard/DashboardIndex";
+import Banner from "../Hub/Banner";
 import LoadingCard from "../Loading/LoadingCard";
 
 const FeaturedIndex = () => {
   return (
-    <Container maxW={"container.xl"}>
-      <Flex flexDir={"column"} gap={4}>
-        <Heading as="h1" fontWeight="bold" fontSize="4xl" mt={10}>
-          Featured Courses
-        </Heading>
-        <Text>Courses submitted by the community </Text>
-        <HStack>
-          <Button>Submit an existing course</Button>
-          <Button>Create a new course</Button>
-        </HStack>
-        <Grid
-          templateColumns={["1fr", "1fr", "repeat(2, 1fr)", "repeat(3, 1fr)"]}
-          gap="1em"
-          mt={10}
-          mb={"200px"}
-        >
-          <CourseCard
-            link={""}
-            title={"hola"}
-            src={FALLBACK_IMG}
-            createdAt={new Date().toString()}
-            cloneButton
-          />
-        </Grid>
-      </Flex>
-    </Container>
+    <>
+      <Banner src="https://images.unsplash.com/photo-1514820720301-4c4790309f46?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80" />
+      <Container maxW={"container.xl"}>
+        <Flex flexDir={"column"} gap={4}>
+          <Heading as="h1" fontWeight="bold" fontSize="4xl" mt={10}>
+            Featured Courses
+          </Heading>
+          <Text>Courses submitted by the community </Text>
+          <HStack>
+            <Button>Submit an existing course</Button>
+            <Button>Create a new course</Button>
+          </HStack>
+          <Grid
+            templateColumns={["1fr", "1fr", "repeat(2, 1fr)", "repeat(3, 1fr)"]}
+            gap="1em"
+            mt={10}
+            mb={"200px"}
+          >
+            <CourseCard
+              link={""}
+              title={"hola"}
+              src={FALLBACK_IMG}
+              createdAt={new Date().toString()}
+              cloneButton
+            />
+          </Grid>
+        </Flex>
+      </Container>
+    </>
   );
 };
 
