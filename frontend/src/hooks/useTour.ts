09101ca11acacc276051165a87df88c8cb6b6b7a
Mauricio Munoz <maumd@fb.com>
2022-08-02 15:02:01 -0700

    adding functions to useTour so it can see in localeStorage if the user has already seen that tour

--- frontend/src/hooks/useTour.ts
+++ frontend/src/hooks/useTour.ts
@@ -1,14 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 interface Steps {
   title: string;
   content: React.ReactNode;
 }
 
-export const useTour = (steps: Steps[]) => {
+const localStorageKey = "learning-u-tour";
+
+export const useTour = (steps: Steps[], key: string) => {
   const [stepNum, setStep] = useState(-1);
+  const [isFinished, setIsFinished] = useState(false);
+
+  useEffect(() => {
+    const tourCompleted = getTourCompletedFromLocalStorage();
+
+    if (!tourCompleted) {
+      saveInitialTourCompletedInLocalStorage();
+      startTour();
+      return;
+    }
+
+    if (!tourCompleted[key]) {
+      startTour();
+      return;
+    }
+
+    setIsFinished(true);
+  }, [key]);
+
+  const getTourCompletedFromLocalStorage = () => {
+    const tourCompleted = localStorage.getItem(localStorageKey);
+
+    if (!tourCompleted) {
+      return null;
+    }
+
+    return JSON.parse(tourCompleted);
+  };
+
+  const saveFinishedInLocalStorage = () => {
+    const tourCompleted = getTourCompletedFromLocalStorage();
+    tourCompleted[key] = true;
+    localStorage.setItem(localStorageKey, JSON.stringify(tourCompleted));
+  };
+
+  const saveInitialTourCompletedInLocalStorage = () => {
+    localStorage.setItem(localStorageKey, JSON.stringify({}));
+  };
 
   const nextStep = () => {
+    if (stepNum + 1 >= steps.length) {
+      saveFinishedInLocalStorage();
+      setIsFinished(true);
+    }
     setStep((step) => step + 1);
   };
 
@@ -25,6 +69,7 @@ export const useTour = (steps: Steps[]) => {
     prevStep,
     nextStep,
     startTour,
+    isFinished,
     currStep: steps[stepNum] || { title: "", content: "" },
   };
 };
