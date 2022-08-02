83ba447ee1cc1c9460b5975ab74d26a45c6fb8ad
Mauricio Munoz <maumd@fb.com>
2022-08-01 17:46:22 -0700

    creating tests to check if login and registration works

---
+++ frontend/cypress/e2e/auth.cy.ts
@@ -0,0 +1,14 @@
+import cypress from "cypress";
+import { uuid } from "../support/commands";
+
+describe("auth", () => {
+  it("Should login a user", () => {
+    cy.login("Test", "mauricio15");
+  });
+  it("Should register a user", () => {
+    const username = uuid();
+    const password = uuid();
+    cy.register(username, password, `${username}@fb.com`);
+    cy.login(username, password);
+  });
+});
