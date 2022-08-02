8d546ada49b2f04200a5d5ed9680907dd592a34a
Mauricio Munoz <maumd@fb.com>
2022-08-02 14:47:19 -0700

    adding tour inside difficulty selector page

--- frontend/src/components/Difficulty/DifficultyIndex.tsx
+++ frontend/src/components/Difficulty/DifficultyIndex.tsx
@@ -1,5 +1,5 @@
 import { Box, Heading } from "@chakra-ui/react";
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams, useSearchParams } from "react-router-dom";
 import { ICourse } from "../../types/course";
 import { useSession, getConfig } from "../../utils/auth";
@@ -8,6 +8,8 @@ import DifficultyCard from "./DifficultyCard";
 import { useQuery } from "react-query";
 import axios from "axios";
 import { FALLBACK_IMG } from "../Dashboard/DashboardIndex";
+import { useTour } from "../../hooks/useTour";
+import GuidedPopover from "../GuidedPopover/GuidedPopover";
 
 const DifficultyIndex = () => {
   const [searchParams] = useSearchParams();
@@ -15,7 +17,21 @@ const DifficultyIndex = () => {
   const name = searchParams.get("name");
 
   const { user, isFetching } = useSession();
+  const { currStep, prevStep, nextStep, stepNum, startTour } = useTour([
+    {
+      title: "Difficulties",
+      content:
+        "When you create a course, it makes two different courses. Try checking both! ",
+    },
+    {
+      title: "Progress",
+      content: "You can also track your progress here! Try reach that 100%",
+    },
+  ]);
 
+  useEffect(() => {
+    startTour();
+  }, []);
   const { data, isLoading } = useQuery(
     `${id}`,
     async () => {
@@ -49,28 +65,37 @@ const DifficultyIndex = () => {
       <Heading as="h1" fontWeight="bold" fontSize="4xl">
         {name}
       </Heading>
-      <Box display="flex" flexDir="column" my="1em" gap="1em" mb="20vh">
-        <DifficultyCard
-          isLoading={isLoading}
-          title="Beginners"
-          progress={progress ? progress["1"] : undefined}
-          src={data?.images[2] ? data?.images[2].small : FALLBACK_IMG}
-          phrase="You can do it!"
-          courseId={id || ""}
-          courseTitle={name || ""}
-          difficulty={1}
-        />
-        <DifficultyCard
-          isLoading={isLoading}
-          title="Advanced"
-          progress={progress ? progress["2"] : undefined}
-          src={data?.images[3] ? data?.images[3].small : FALLBACK_IMG}
-          courseId={id || ""}
-          courseTitle={name || ""}
-          phrase="Let's go"
-          difficulty={2}
-        />
-      </Box>
+      <GuidedPopover
+        content={currStep.content}
+        title={currStep.title}
+        isOpen={stepNum === 0 || stepNum === 1}
+        prevStep={prevStep}
+        onClose={nextStep}
+      >
+        <Box display="flex" flexDir="column" my="1em" gap="1em" mb="20vh">
+          <DifficultyCard
+            isLoading={isLoading}
+            title="Beginners"
+            progress={progress ? progress["1"] : undefined}
+            src={data?.images[2] ? data?.images[2].small : FALLBACK_IMG}
+            phrase="You can do it!"
+            courseId={id || ""}
+            courseTitle={name || ""}
+            difficulty={1}
+          />
+
+          <DifficultyCard
+            isLoading={isLoading}
+            title="Advanced"
+            progress={progress ? progress["2"] : undefined}
+            src={data?.images[3] ? data?.images[3].small : FALLBACK_IMG}
+            courseId={id || ""}
+            courseTitle={name || ""}
+            phrase="Let's go"
+            difficulty={2}
+          />
+        </Box>
+      </GuidedPopover>
     </>
   );
 };
