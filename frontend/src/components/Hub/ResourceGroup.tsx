260fda7af4051d304369f7e99738ac82432a1356
Mauricio Munoz <maumd@fb.com>
2022-08-02 14:46:04 -0700

    adding tour inside hub video dashboard

--- frontend/src/components/Hub/ResourceGroup.tsx
+++ frontend/src/components/Hub/ResourceGroup.tsx
@@ -1,6 +1,8 @@
-import { Box, Grid, Heading } from "@chakra-ui/react";
-import React from "react";
+import { Badge, Box, Grid, Heading, Text } from "@chakra-ui/react";
+import React, { useEffect } from "react";
+import { useTour } from "../../hooks/useTour";
 import { IResource } from "../../types/resource";
+import GuidedPopover from "../GuidedPopover/GuidedPopover";
 import DocsCard from "./DocsCard";
 import VideoCard from "./VideoCard";
 
@@ -13,38 +15,79 @@ interface Props {
 }
 
 const ResourceGroup = ({ title, kind, data, courseId, courseName }: Props) => {
+  const { currStep, prevStep, nextStep, stepNum, startTour } = useTour([
+    {
+      title: "Video Tutorials!",
+      content:
+        "These are the videos that were generated. Try to watch them all!",
+    },
+    {
+      title: "In progress status",
+      content: "Clicking a video will automatically mark it as In Progress.",
+    },
+    {
+      title: "Finished status",
+      content: (
+        <Text>
+          Click on this <Badge colorScheme="blue">In progress</Badge> badge to
+          mark the video as <Badge colorScheme={"green"}>Completed</Badge>.
+        </Text>
+      ),
+    },
+  ]);
+
+  useEffect(() => {
+    startTour();
+  }, []);
+
   return (
     <Box>
-      <Heading as="h2" fontSize="xl" mb={3}>
-        {title}
-      </Heading>
-      {/* See https://chakra-ui.com/docs/styled-system/responsive-styles */}
-      <Grid
-        templateColumns={["1fr", "1fr", "repeat(2, 1fr)", "repeat(3, 1fr)"]}
-        gap="1em"
+      <GuidedPopover
+        content={currStep.content}
+        title={currStep.title}
+        isOpen={stepNum === 0}
+        prevStep={prevStep}
+        onClose={nextStep}
       >
-        {kind === "video" &&
-          data &&
-          data.map((resource) => (
-            <VideoCard
-              objectId={resource.objectId}
-              key={resource.objectId}
-              title={resource.title}
-              src={resource.type === "video" ? resource.thumbnail : ""}
-              status={resource.status}
-              href={resource.url}
-              courseId={courseId}
-              courseName={courseName}
-            />
-          ))}
-        {kind === "documentation" && (
-          <>
-            <DocsCard title="React beginner tutorial" href="yes" />
-            <DocsCard title="React beginner tutorial" href="yes" />
-            <DocsCard title="React beginner tutorial" href="yes" />
-          </>
-        )}
-      </Grid>
+        <Heading as="h2" fontSize="xl" mb={3}>
+          {title}
+        </Heading>
+      </GuidedPopover>
+      <GuidedPopover
+        content={currStep.content}
+        title={currStep.title}
+        isOpen={stepNum === 1 || stepNum === 2}
+        prevStep={prevStep}
+        onClose={nextStep}
+      >
+        {/* See https://chakra-ui.com/docs/styled-system/responsive-styles */}
+        <Grid
+          templateColumns={["1fr", "1fr", "repeat(2, 1fr)", "repeat(3, 1fr)"]}
+          gap="1em"
+        >
+          {kind === "video" &&
+            data &&
+            data.map((resource) => (
+              <VideoCard
+                key={resource.objectId}
+                objectId={resource.objectId}
+                title={resource.title}
+                src={resource.type === "video" ? resource.thumbnail : ""}
+                status={resource.status}
+                href={resource.url}
+                courseId={courseId}
+                courseName={courseName}
+              />
+            ))}
+          {kind === "documentation" && (
+            <>
+              <DocsCard title="React beginner tutorial" href="yes" />
+              <DocsCard title="React beginner tutorial" href="yes" />
+              <DocsCard title="React beginner tutorial" href="yes" />
+            </>
+          )}
+        </Grid>
+      </GuidedPopover>
     </Box>
   );
 };
