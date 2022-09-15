d4b530ddb0c4821554cccbf079236f41ce21dfc3
Mau MD <j.mauricio.munoz1@gmail.com>
2022-09-15 15:05:35 -0700

    manually adding cors configuration to avoid cors error on production

--- backend/app.ts
+++ backend/app.ts
@@ -21,7 +21,18 @@ Parse.serverURL = process.env.PARSE_SERVER_URL;
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 app.use("/auth", auth);
 app.use("/debug", debug);
